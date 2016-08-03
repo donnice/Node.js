8c1d387561a98186e04e42ad7e526ae3596d58a7
Hai Tang <402696415@qq.com>
2016-08-02 21:19:55 -0500

    POST

--- chapter4/post-41.js
+++ chapter4/post-41.js
@@ -1,3 +1,19 @@
-/**
- * http://usejsdoc.org/
- */
+var http = require('http');
+var url = require('url');
+var items = [];
+
+var server = http.createServer(function(req,res){
+	switch(req.method){
+	case 'POST':
+		var item = '';
+		req.setEncoding('utf8');
+		req.on('data',function(chunk){
+			item += chunk;
+		});
+		req.on('end',function(){
+			item.push(item);
+			res.end('OK\n');
+		});
+		break;
+	}
+})
